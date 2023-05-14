04cb5c8d940e07ea9231b936a3d42d5073768bf2
fabinho190 <ossai10fabianfbn@gmail.com>
2023-05-14 20:20:41 +0200

    Login completo

--- js/login.js
+++ js/login.js
@@ -1,43 +1,52 @@
-const wrapper = document.querySelector('.wrapper');
-const welcome = document.querySelector('.welcome');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconeClose = document.querySelector('.icon-close');
+const wrapper = document.querySelector(".wrapper");
+const welcome = document.querySelector(".welcome");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const iconeClose = document.querySelector(".icon-close");
 
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-    welcome.classList.remove('active-popup');
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+  welcome.classList.remove("active-popup");
 });
 
-iconeClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
+iconeClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
 });
 
-function enviar(){
-    // valores de los campos de entrada de email y password en el formulario y lo guarda en la variable email y password respectivamente
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    //Proceso Backend
-    //Inicia la solicitud hhtp
-    var http = new XMLHttpRequest();
-    // Configuración de  la solicitud HTTP GET al servlet .
-    http.open("GET","http://localhost:8080/farmaceutica/login?email=" + email +"&password="+ password,true);
-    //Define una función de devolución de llamada que se ejecutará cuando la respuesta de la solicitud HTTP esté lista.
-    http.onreadystatechange = function(){
-        // Verifica si el estado de la respuesta es 200 ( que signica éxito) para continuar .
-        if(this.readyState == 4 && http.status==200){
-                //Obtiene el texto de la respuesta de la solicitud HTTP
-                var session = http.responseText;
-                if(session !==null){
-                    //Almacena el valor de la sesión en el almacenamiento de sesión del navegador y lo mismo pasa con el email
-                    sessionStorage.setItem("session",session)
-                    sessionStorage.setItem("mail",email);
-                    // Se pone página de gestión,ya que el login ha sido un exito
-                    window.location.href ="paginas/gestion.html";
-                } else{
-                    alert("Sesion interrumpida.Vuelva a intentarlo.")
-                }
-            }
-    }
-    //Envía la solicitud HTTP al servidor con los parámetros especificados en el método open()*/
-    http.send();
+
+function enviar() {
+    // Obtiene los valores de los campos de entrada de email y password en el formulario
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+  
+    // Construye la URL de la solicitud con los valores codificados de email y password
+    const url = `http://localhost:8080/farmaceutica/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+  
+    // Realiza la solicitud usando fetch API
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          // Si la respuesta de la solicitud es exitosa, devuelve el texto de la respuesta
+          return response.text();
+        } else {
+          // Si la respuesta no es exitosa, lanza un error
+          throw new Error("Error en la solicitud de inicio de sesión.");
+        }
+      })
+      .then(session => {
+        if (session !== null) {
+          // Si se obtiene una sesión válida, la guarda en el almacenamiento de sesión del navegador
+          sessionStorage.setItem("session", session);
+          sessionStorage.setItem("mail", email);
+          // Redirecciona a la página de gestión
+          window.location.href = "paginas/gestion.html";
+        } else {
+          // Si la sesión es nula, lanza un error
+          throw new Error("Sesión interrumpida. Vuelva a intentarlo.");
+        }
+      })
+      .catch(error => {
+        // Captura cualquier error ocurrido durante el proceso y muestra una alerta
+        console.error(error);
+        alert(error.message);
+      });
 }
