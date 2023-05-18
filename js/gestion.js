496185bb494c6cd24a8fbf5bc5f99ba549881d25
fabinho190 <ossai10fabianfbn@gmail.com>
2023-05-18 23:29:37 +0200

    ver tabla prototipo

--- js/gestion.js
+++ js/gestion.js
@@ -13,7 +13,20 @@ function verTabla(){
   let session = sessionStorage.getItem('session');
   var http = new XMLHttpRequest();
   http.open("GET","http://localhost:8080/farmaceutica/ServXips?mail=" + mail +"&session="+session, true);
+
+  http.onload = function(){
+    if(http.status === 200  ){
+      let responseHtml = http.response;
+      let tableContainer = document.getElementById("table-container");
+      tableContainer.innerHTML = responseHtml;
+      tableContainer.style.display = "block"
+    } else{
+      console.error("Error en la solicitud"+ http.status);
+    }
+  }
   http.send();
 
 }
-  
+window.onload = function(){
+  document.querySelector(".icon.tabla .two").onclick = verTabla;
+}
