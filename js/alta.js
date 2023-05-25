289b355942e1f5c3a03ddf017e46693e9b3fc43b
fabinho190 <ossai10fabianfbn@gmail.com>
2023-05-26 00:19:35 +0200

    Alta obtener Pacientes

--- js/alta.js
+++ js/alta.js
@@ -1,3 +1,28 @@
 function irGestion (){
     window.location.href = "../paginas/gestion.html";
 }
+
+function getPatients(){
+    let mail = sessionStorage.getItem('mail');
+    let session = sessionStorage.getItem('session');
+    var http = new XMLHttpRequest();
+    
+    http.open("GET","http://localhost:8080/farmaceutica/ServePatients?mail=" + mail + "&session="+ session,true);
+
+    http.onload = function (){
+        if (this.readyState== 4 && http.status==200){
+            var response = JSON.parse(http.response);
+
+            var patientSelect = document.getElementById("paciente");
+            for(var i = 0; i < response.length; i++){
+                var option = document.createElement("option");
+                option.text = response[i];
+                patientSelect.add(option);
+            }  
+        }else{
+            console.error("Error en la solicitud ServePatients",http.status);
+        }
+    };
+
+    http.send();
+}
