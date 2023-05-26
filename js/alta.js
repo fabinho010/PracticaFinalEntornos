c8df571a7e48acbf880c86dd7103012b264e76cd
fabinho190 <ossai10fabianfbn@gmail.com>
2023-05-26 05:54:39 +0200

    selects

--- js/alta.js
+++ js/alta.js
@@ -12,17 +12,66 @@ function getPatients(){
     http.onload = function (){
         if (this.readyState== 4 && http.status==200){
             var response = JSON.parse(http.response);
-
             var patientSelect = document.getElementById("paciente");
+          // Limpiar el select antes de agregar las nuevas opciones
+            patientSelect.innerHTML= "";
+
+            // Agregar la opción "Elige un medicamento..."
+            var defaultOption = document.createElement("option");
+            defaultOption.selected = true;
+            defaultOption.disabled = true;
+            defaultOption.text = "Elige un paciente...";
+            patientSelect.add(defaultOption);
+
+            //Agrego las opciones
             for(var i = 0; i < response.length; i++){
                 var option = document.createElement("option");
                 option.text = response[i];
+                option.value = response[i];
                 patientSelect.add(option);
             }  
+            
         }else{
             console.error("Error en la solicitud ServePatients",http.status);
         }
     };
-
     http.send();
 }
+
+function getMedicamentos(){
+        let mail = sessionStorage.getItem('mail');
+        let session = sessionStorage.getItem('session');
+        var http = new XMLHttpRequest();
+        http.open("GET","http://localhost:8080/farmaceutica/ServeMedicines?mail=" + mail + "&session="+ session,true);
+
+        http.onload = function(){
+            if (this.readyState== 4 && http.status==200){
+                var reponse = JSON.parse(http.responseText);
+
+                var medicinaSelect = document.getElementById("medicamento");
+
+                // Limpiar el select antes de agregar las opciones
+                medicinaSelect.innerHTML = "";
+
+                // Agregar la opción "Elige un medicamento..."
+                var defaultOption = document.createElement("option");
+                defaultOption.selected = true;
+                defaultOption.disabled = true;
+                defaultOption.text = "Elige un medicamento...";
+                medicinaSelect.add(defaultOption);
+                //Agrego las opciones
+                for(var i = 0; i < reponse.length; i++){
+                    var option = document.createElement("option");
+                    option.text = reponse[i].id;
+                    option.value = reponse[i].id;
+                    medicinaSelect.add(option);
+                }
+                
+        }else{
+            console.error("Error en la solicitud ServeMedicines",http.status);
+        }
+    };
+    http.send();
+}
+    getMedicamentos();
+    getPatients();
