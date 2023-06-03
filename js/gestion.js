f4d3a7f32bca1bed427da366a800bdc9ba033225
fabinho190 <ossai10fabianfbn@gmail.com>
2023-06-03 14:41:46 +0200

    Fin

--- js/gestion.js
+++ js/gestion.js
@@ -3,12 +3,14 @@ function irAlta() {
   window.location.href = "../paginas/alta.html";
 }
 
+/**ME PERMITE IR LA PAGINA DE lOGIN */
 function irLogin() {
     sessionStorage.removeItem('session');
     sessionStorage.removeItem('mail');
     window.location.href = "../index.html";
 }
 
+/* mE ENSEÑA LA TABLA DE MEDICAMENTOS*/
 function verTabla(){
   let mail = sessionStorage.getItem('mail');
   let session = sessionStorage.getItem('session');
@@ -20,7 +22,11 @@ function verTabla(){
       let responseHtml = http.response;
       let containerButtons =  document.querySelector(".container-buttons");
       let tableContainer = document.getElementById("table-container");
+      /**
+      Meto la repuesta del backend en el div correspondiente a la tabla
+       */
       tableContainer.innerHTML = responseHtml;
+      //Eventos a la hora de mostar la tabla,el bloque de botones se va hacia arriba y se enseña la tabla
       tableContainer.classList.add("show");
       containerButtons.classList.add("arriba");
     } else{
